9832ae74610d286a48c4856d2436e2f320aa0b27
Kimber Herron <kh582013@gmail.com>
2022-09-21 15:39:43 -0400

    commit tree 6269dfa52b464da16d79616fb38c0b0f0b183e87

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = (parameter1, parameter2) => parameter1 / parameter2;
+let parameter1 = 2000
+let parameter2 = 100
+
+const square = (parameter3) => parameter3 * parameter3;
+let parameter3 = 2
+
+const add = (parameter4, parameter5) => parameter4 + parameter5;
+let parameter4 = 3
+let parameter5 = 4
